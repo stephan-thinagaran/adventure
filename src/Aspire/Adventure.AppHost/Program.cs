089f33eff302b2a78f83a397c2edd44d8894ecea
stephan-thinagaran <stephan.thinagaran@live.com>
2025-04-14 07:30:10 +0530

    Fixed github issues

--- src/Aspire/Adventure.AppHost/Program.cs
+++ src/Aspire/Adventure.AppHost/Program.cs
@@ -1,14 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var redisCache = builder.AddRedis("redisCache");
+//var redisCache = builder.AddRedis("redisCache");
 
 var adventureWorkDb = builder.AddSqlServer("AdventureWorks2019")
                              .AddDatabase("db", "AdventureWorks2022");
 
 var personService = builder.AddProject<Projects.Adventure_AdventureWork>("adventure-work")
                            .WithExternalHttpEndpoints()
-                           .WithReference(redisCache)
-                           .WaitFor(redisCache)
+                           //.WithReference(redisCache)
+                           //.WaitFor(redisCache)
                            .WithReference(adventureWorkDb)
                            .WaitFor(adventureWorkDb);
                            
